3562427865e5b4fbc0a2f22c4eee49e7b7886724
Vasily Krakovetsky <vasis123@gmail.com>
2021-04-22 18:45:54 +0300

    add types

---
+++ dist/src/index.d.ts
@@ -0,0 +1,16 @@
+/// <reference types="node" />
+declare const defaultOptions: {
+    threshold: number;
+};
+interface inputImage {
+    data: Buffer | Uint8Array | Uint8ClampedArray;
+    width: number;
+    height: number;
+}
+interface outputCoords {
+    x: number;
+    y: number;
+}
+export default function subImageMatch(img: inputImage, subImg: inputImage, optionsParam?: typeof defaultOptions): boolean | outputCoords;
+export declare function subImageMatchOnCoordinates(img: inputImage, subImg: inputImage, matchY: number, matchX: number, maxDelta: number): boolean;
+export {};
